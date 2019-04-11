a40251c193a3097741462ac4a63e488845abbae0
Katesold <katalap@gmail.com>
2019-04-11 16:32:46 +0100

    working php mail but goes to spam

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$formcontent="From: $name \n\n Phone: $phone \n\n Message: $message";
+$recipient = "kata@22design.co.uk, $email";
+$subject = "Contact Form";
+//$headers = 'From: info@aequos.co.uk, Organisation: Aequos';
+// $headers .= "Organization: Sender Organization\r\n";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader, $message) or die("Error!");
+echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+?>
--- main.js
+++ main.js
@@ -56,6 +56,7 @@ function screenTest(e) {
       nav[0].style.display = "None";
     } else {
       nav[0].style.display = "flex";
+      navBg.style.height = "0";
     }
 }
   
@@ -181,18 +182,38 @@ function switchReasonsL(){
 
 
 
-// $(document).ready(function(){   
-//     setTimeout(function () {
-//         $("#CONSTANT_OPEN_URL").fadeIn(200);
-//      }, 4000);
-//     $("#CONSTANT_OPEN_URL, .cli-plugin-main-link").click(function() {
-//         $("#CONSTANT_OPEN_URL").fadeOut(200);
-//     }); 
-// }); 
-
-
-// $(document).ready(function(){
-//     $("section").click(function(){
-//       $("section").hide();
-//     });
-//   });
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
