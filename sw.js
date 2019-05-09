f1496859ab8a9e9f9ad696c728c41d22080be1fd
Katesold <katalap@gmail.com>
2019-05-09 12:23:17 +0100

    minified and prefixed files

--- sw.js
+++ sw.js
@@ -1,14 +1,3 @@
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', function() {
-//       navigator.serviceWorker.register('/sw.js').then(function(registration) {
-//         // Registration was successful
-//         console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//       }, function(err) {
-//         // registration failed :(
-//         console.log('ServiceWorker registration failed: ', err);
-//       });
-//     });
-//   }
 
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
